398e8fe6a2ebb20b429f2e830676d3ff0048db72
Igor Khomyakov <ikh@ikhsoftware.com>
2025-12-04 07:41:46 +0000

    Fized default shm names, added more args to bench

--- examples/bench.rs
+++ examples/bench.rs
@@ -14,9 +14,9 @@ use std::{
 #[cfg(not(unix))]
 compile_error!("This crate only supports Unix-like operating systems.");
 
-const BUFFER_LEN: usize = 1 << 16;
+const DEFAULT_N_TRIALS: usize = 100_000;
+const DEFAULT_BUFFER_LENGTH: usize = 1 << 16;
 const PAYLOAD_SIZE: usize = 392;
-const TRIALS: usize = 100_000;
 
 /// Example payload type used in each ring-buffer slot (wrapped in a `Message`).
 #[derive(Clone, Copy, Debug)]
@@ -41,6 +41,14 @@ struct Args {
     /// Operation mode, selected via subcommand.
     #[command(subcommand)]
     command: Commands,
+
+    /// Number of messages to process.
+    #[arg(short = 't', long = "trials", default_value_t = DEFAULT_N_TRIALS, value_name = "COUNT")]
+    n_trials: usize,
+
+    /// Channel buffer length (in slots).
+    #[arg(short = 'l', long = "buffer-length", default_value_t = DEFAULT_BUFFER_LENGTH, value_name = "LENGTH")]
+    buffer_length: usize,
 }
 
 #[derive(Subcommand, Debug)]
@@ -52,14 +60,14 @@ enum Commands {
         period: u64,
 
         /// Output shared-memory segment name, which must begin with ‘/’.
-        #[arg(short = 'o', long = "output", default_value = "test-reader-writer")]
+        #[arg(short = 'o', long = "output", default_value = "/test-reader-writer")]
         output: String,
     },
 
     /// Receives messages from a shared-memory channel.
     Reader {
         /// Input shared-memory segment name, which must begin with ‘/’.
-        #[arg(short = 'i', long = "input", default_value = "test-reader-writer")]
+        #[arg(short = 'i', long = "input", default_value = "/test-reader-writer")]
         input: String,
     },
 
@@ -71,11 +79,11 @@ enum Commands {
         period: u64,
 
         /// Output shared-memory segment name, which must begin with ‘/’.
-        #[arg(short = 'o', long = "output", default_value = "test-client-server")]
+        #[arg(short = 'o', long = "output", default_value = "/test-client-server")]
         output: String,
 
         /// Input shared-memory segment name, which must begin with ‘/’.
-        #[arg(short = 'i', long = "input", default_value = "test-server-client")]
+        #[arg(short = 'i', long = "input", default_value = "/test-server-client")]
         input: String,
     },
 
@@ -83,11 +91,11 @@ enum Commands {
     /// them onto an output shared-memory channel.
     Server {
         /// Input shared-memory segment name, which must begin with ‘/’.
-        #[arg(short = 'i', long = "input", default_value = "test-client-server")]
+        #[arg(short = 'i', long = "input", default_value = "/test-client-server")]
         input: String,
 
         /// Output shared-memory segment name, which must begin with ‘/’.
-        #[arg(short = 'o', long = "output", default_value = "test-server-client")]
+        #[arg(short = 'o', long = "output", default_value = "/test-server-client")]
         output: String,
     },
 
@@ -112,48 +120,68 @@ enum Commands {
     /// Receives messages from a shared-memory channel and saves them to a file.
     Logger {
         /// Input shared-memory segment name, which must begin with ‘/’.
-        #[arg(short = 'i', long = "input", default_value = "test-reader-writer")]
+        #[arg(short = 'i', long = "input", default_value = "/test-reader-writer")]
         input: String,
 
-        /// Input shared-memory segment name, which must begin with ‘/’.
-        #[arg(short = 'f', long = "file")]
+        /// Output file path
+        #[arg(short = 'f', long = "file", value_name = "PATH")]
         path: PathBuf,
+
+        /// Logger buffer size in MBytes
+        #[arg(
+            short = 's',
+            long = "buffer-size",
+            default_value_t = 8,
+            value_name = "SIZE"
+        )]
+        logger_buffer_size: usize,
     },
 }
 
 fn main() -> std::io::Result<()> {
     let args = Args::parse();
 
+    let buffer_length = args.buffer_length;
+    let n_trials = args.n_trials;
+
     println!(
         "message size: {}, payload size: {}, cache padded payload size: {}, buffer length: {}, buffer size: {}, trials: {}",
         std::mem::size_of::<Message<Payload<PAYLOAD_SIZE>>>(),
         std::mem::size_of::<Payload<PAYLOAD_SIZE>>(),
         std::mem::size_of::<CachePadded<Payload<PAYLOAD_SIZE>>>(),
-        BUFFER_LEN,
-        std::mem::size_of::<Message<Payload<PAYLOAD_SIZE>>>() * BUFFER_LEN,
-        TRIALS,
+        buffer_length,
+        std::mem::size_of::<Message<Payload<PAYLOAD_SIZE>>>() * buffer_length,
+        n_trials,
     );
 
     match args.command {
         Commands::Writer { period, output } => {
             println!("period: {}, output: {:?}", period, output);
-            let (tx, _) = channel(&output, BUFFER_LEN)?;
-            writer(tx, period)
+            let (tx, _) = channel(&output, buffer_length)?;
+            writer(tx, period, n_trials)
         }
 
         Commands::Reader { input } => {
             println!("input: {:?}", input);
-            let (_, rx) = channel(&input, BUFFER_LEN)?;
-            reader(rx)
+            let (_, rx) = channel(&input, buffer_length)?;
+            reader(rx, n_trials)
         }
 
-        Commands::Logger { input, path } => {
-            println!("input: {:?}, output file: {:?}", input, path);
-            let (_, rx) = channel(&input, BUFFER_LEN)?;
+        Commands::Logger {
+            input,
+            path,
+            logger_buffer_size,
+        } => {
+            let logger_buffer_size = logger_buffer_size << 20;
+            println!(
+                "input: {:?}, output file: {:?}, logger buffer size: {}",
+                input, path, logger_buffer_size
+            );
+            let (_, rx) = channel(&input, buffer_length)?;
             let file = OpenOptions::new().create(true).append(true).open(&path)?;
-            let writer = BufWriter::with_capacity(1 << 20, file);
+            let writer = BufWriter::with_capacity(logger_buffer_size, file);
 
-            logger(rx, writer)
+            logger(rx, writer, n_trials)
         }
 
         Commands::Client {
@@ -165,15 +193,15 @@ fn main() -> std::io::Result<()> {
                 "period: {}, output: {:?}, input: {:?}",
                 period, output, input
             );
-            let (tx1, _) = channel(output, BUFFER_LEN)?;
-            let (_, rx2) = channel(input, BUFFER_LEN)?;
-            client(tx1, rx2, period)
+            let (tx1, _) = channel(output, buffer_length)?;
+            let (_, rx2) = channel(input, buffer_length)?;
+            client(tx1, rx2, period, n_trials)
         }
 
         Commands::Server { input, output } => {
             println!("output: {:?}, input: {:?}", output, input);
-            let (_, rx1) = channel(input, BUFFER_LEN)?;
-            let (tx2, _) = channel(output, BUFFER_LEN)?;
+            let (_, rx1) = channel(input, buffer_length)?;
+            let (tx2, _) = channel(output, buffer_length)?;
             server(tx2, rx1)
         }
 
@@ -181,7 +209,7 @@ fn main() -> std::io::Result<()> {
             period,
             max_readers,
         } => {
-            let (tx, rx) = local_channel(BUFFER_LEN);
+            let (tx, rx) = local_channel(buffer_length);
 
             let cores = core_affinity::get_core_ids().unwrap();
             assert!(
@@ -202,7 +230,7 @@ fn main() -> std::io::Result<()> {
                     let core_id = cores[i].clone();
                     std::thread::spawn(move || {
                         core_affinity::set_for_current(core_id);
-                        reader(rx)
+                        reader(rx, n_trials)
                     })
                 })
                 .collect();
@@ -210,7 +238,7 @@ fn main() -> std::io::Result<()> {
             let core_id = cores[1].clone();
             let writer = std::thread::spawn(move || {
                 core_affinity::set_for_current(core_id);
-                writer(tx, period)
+                writer(tx, period, n_trials)
             });
 
             readers
@@ -225,8 +253,8 @@ fn main() -> std::io::Result<()> {
         Commands::Ping { period } => {
             println!("period: {}", period);
 
-            let (tx1, rx1) = local_channel(BUFFER_LEN);
-            let (tx2, rx2) = local_channel(BUFFER_LEN);
+            let (tx1, rx1) = local_channel(buffer_length);
+            let (tx2, rx2) = local_channel(buffer_length);
 
             let cores = core_affinity::get_core_ids().unwrap();
             assert!(
@@ -244,7 +272,7 @@ fn main() -> std::io::Result<()> {
             let core_id = cores[2].clone();
             let client = std::thread::spawn(move || {
                 core_affinity::set_for_current(core_id);
-                client(tx1, rx2, period)
+                client(tx1, rx2, period, n_trials)
             });
 
             client.join().unwrap()?;
@@ -259,9 +287,10 @@ fn client(
     tx: Sender<Payload<PAYLOAD_SIZE>, impl Buffer<Payload<PAYLOAD_SIZE>>>,
     mut rx: Receiver<Payload<PAYLOAD_SIZE>, impl Buffer<Payload<PAYLOAD_SIZE>>>,
     period: u64,
+    n_trials: usize,
 ) -> std::io::Result<()> {
-    let mut trials: Trials<u64> = Trials::with_capacity(TRIALS);
-    let mut trials2: Trials<u64> = Trials::with_capacity(TRIALS);
+    let mut trials: Trials<u64> = Trials::with_capacity(n_trials);
+    let mut trials2: Trials<u64> = Trials::with_capacity(n_trials);
 
     let mut payload = Payload::<PAYLOAD_SIZE>::default();
 
@@ -277,7 +306,7 @@ fn client(
         let ts3 = delay(ts0 + period);
         trials2.push(ts3 - ts0);
 
-        if trials.len() == TRIALS {
+        if trials.len() == n_trials {
             break;
         }
     }
@@ -316,10 +345,11 @@ fn delay(deadline_ns: u64) -> u64 {
 fn writer(
     tx: Sender<Payload<PAYLOAD_SIZE>, impl Buffer<Payload<PAYLOAD_SIZE>>>,
     period: u64,
+    n_trials: usize,
 ) -> std::io::Result<()> {
-    let mut trials = Trials::with_capacity(TRIALS);
-    let mut trials2 = Trials::with_capacity(TRIALS);
-    let mut trials3 = Trials::with_capacity(TRIALS);
+    let mut trials = Trials::with_capacity(n_trials);
+    let mut trials2 = Trials::with_capacity(n_trials);
+    let mut trials3 = Trials::with_capacity(n_trials);
 
     let mut payload = Payload::<PAYLOAD_SIZE>::default();
 
@@ -335,7 +365,7 @@ fn writer(
         let ts3 = delay(ts0 + period);
         trials3.push(ts3 - ts0);
 
-        if trials.len() == TRIALS {
+        if trials.len() == n_trials {
             break;
         }
     }
@@ -353,9 +383,10 @@ fn writer(
 #[inline(never)]
 fn reader(
     mut rx: Receiver<Payload<PAYLOAD_SIZE>, impl Buffer<Payload<PAYLOAD_SIZE>>>,
+    n_trials: usize,
 ) -> std::io::Result<()> {
-    let mut trials = Trials::with_capacity(TRIALS);
-    let mut trials2 = Trials::with_capacity(TRIALS);
+    let mut trials = Trials::with_capacity(n_trials);
+    let mut trials2 = Trials::with_capacity(n_trials);
 
     let mut prev_seq_no: u64 = 0;
     loop {
@@ -392,7 +423,7 @@ fn reader(
             );
         }
         prev_seq_no = seq_no;
-        if trials.len() == TRIALS {
+        if trials.len() == n_trials {
             break;
         }
     }
@@ -413,15 +444,16 @@ fn as_bytes<T: Sized>(p: &T) -> &[u8] {
 fn logger(
     mut rx: Receiver<Payload<PAYLOAD_SIZE>, impl Buffer<Payload<PAYLOAD_SIZE>>>,
     mut writer: BufWriter<File>,
+    n_trials: usize,
 ) -> std::io::Result<()> {
-    let mut trials = Trials::with_capacity(TRIALS);
-    let mut trials2 = Trials::with_capacity(TRIALS);
-    let mut trials3 = Trials::with_capacity(TRIALS);
+    let mut trials = Trials::with_capacity(n_trials);
+    let mut trials2 = Trials::with_capacity(n_trials);
+    let mut trials3 = Trials::with_capacity(n_trials);
 
     let mut prev_seq_no: u64 = 0;
     loop {
         let ts0 = mono_time_ns();
-        
+
         let (seq_no, payload) = rx.recv();
         let ts1 = payload.timestamp;
 
@@ -444,7 +476,7 @@ fn logger(
             );
         }
         prev_seq_no = seq_no;
-        if trials.len() == TRIALS {
+        if trials.len() == n_trials {
             break;
         }
     }
